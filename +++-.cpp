60947d5dae6ced22e58ee8977992baf9fa464f27
As190704 <133184716+As190704@users.noreply.github.com>
2024-09-11 22:20:57 +0530

    Add files via upload

---
+++ +++-.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{  float a=20,b=158;
+	printf("%f,%f,%f,%f,%f,%f\n", a,b, b/a,a+b,a-b,a*b/(a+b)  );
+	printf("Anurag earned:%f",b*b*b*b*b);
+	
+	
+}
+
